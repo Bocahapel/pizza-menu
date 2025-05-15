0be566fc3a4fa09d8cf06962dfb6caa8da5206f4
Bocahapel <pratamadrian.putra@gmail.com>
2025-05-15 17:10:47 +0700

    add pizza images and css

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from "react";
-import ReactDOM, { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
 
 function App() {
   return <h1>Hello React!</h1>;
 }
 //latest render react since version 18
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
